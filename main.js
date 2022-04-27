b079c1b268b38148cf06d5a135adc05a540cd595
grinsekatze <violetta.stix@gmail.com>
2022-04-27 14:35:56 +0200

    Redoing code:Basic operating functions

--- main.js
+++ main.js
@@ -1,41 +1,23 @@
-const buttonContainer = document.querySelector(".button-container");
-const operatorButtons = document.querySelectorAll(".operator");
-const display = document.querySelector(".display");
+let a = parseInt(prompt("Input a number:"));
+let b = parseInt(prompt("Input a number:"));
 
-
-// Create one button for each number
-function createButtons(){
-    for (let i = 0; i <= 9; i++) {
-        const buttons = document.createElement('button');
-        buttons.classList.add('numbers');
-        buttons.textContent = i;
-        buttons.value = i;
-        buttonContainer.appendChild(buttons);
-        buttons.addEventListener("click", () => {
-            display.textContent = i
-            //operate(buttons.value)
-        });
-    }
+function add(a, b){
+    console.log(a + b);
 }
 
-/*function operate(num) {
-    let num1 = num;
-    console.log(num1);
-}*/
-
-function operator(op) {
-    let num1 = 1;
-    let num2 = 2;
-    if (op === '+') {
-        return num1 + num2;
-    }
+function substract(a, b){
+    console.log(a - b);
 }
 
-createButtons();
+function multiply(a, b){
+    console.log(a * b);
+}
 
-operatorButtons.forEach( btn =>
-    btn.addEventListener("click", () => {
-        display.textContent = btn.value
-        operator(btn.value)
-    })
-);
+function divide(a, b){
+    console.log((a/b).toFixed(10));
+}
+
+add(a, b);
+substract(a, b);
+multiply(a, b);
+divide(a, b);

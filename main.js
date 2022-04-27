67ceecc2bf35ae29e12c95319cf85b6070ce892f
grinsekatze <violetta.stix@gmail.com>
2022-04-27 14:15:10 +0200

    Attempt at making Calculator function, not working yet

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 const buttonContainer = document.querySelector(".button-container");
+const operatorButtons = document.querySelectorAll(".operator");
+const display = document.querySelector(".display");
+
 
 // Create one button for each number
-for (let i = 0; i <= 9; i++) {
-    const buttons = document.createElement('button');
-    buttons.classList.add('numbers');
-    buttons.textContent = i;
-    buttonContainer.appendChild(buttons);
+function createButtons(){
+    for (let i = 0; i <= 9; i++) {
+        const buttons = document.createElement('button');
+        buttons.classList.add('numbers');
+        buttons.textContent = i;
+        buttons.value = i;
+        buttonContainer.appendChild(buttons);
+        buttons.addEventListener("click", () => {
+            display.textContent = i
+            //operate(buttons.value)
+        });
+    }
 }
 
+/*function operate(num) {
+    let num1 = num;
+    console.log(num1);
+}*/
 
-
-function operate(num1, num2) {
-
+function operator(op) {
+    let num1 = 1;
+    let num2 = 2;
+    if (op === '+') {
+        return num1 + num2;
+    }
 }
+
+createButtons();
+
+operatorButtons.forEach( btn =>
+    btn.addEventListener("click", () => {
+        display.textContent = btn.value
+        operator(btn.value)
+    })
+);

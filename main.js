8583d15a6ecf705c20c1238119fe652396fe9fe3
grinsekatze <violetta.stix@gmail.com>
2022-05-03 09:49:40 +0200

    Calculation now works if the input contains multiple operations: - After pressing enter - After each operator

--- main.js
+++ main.js
@@ -1,67 +1,174 @@
-/*let a = parseInt(prompt("Input a number:"));
-let b = parseInt(prompt("Input a number:"));
-let operator = prompt("Please input an operator: +, -, *, /")
-*/
 const operatorBtn = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".numbers");
 const enterBtn = document.querySelector(".enter");
-const display = document.querySelector(".display");
+const input = document.querySelector(".input");
+const result = document.querySelector(".input");
+const clearBtn = document.querySelector(".clear");
 
-let numberContainer = [];
-let inputNumber = '';
+/*
+let previousNum = '';
+let currentNum = '';
 let operator = '';
 
 
-function readValue(e){
-    display.textContent = e.innerText;
-    inputNumber += e.innerText;
-    console.log(inputNumber);
+function readInput(num){
+    //assign currentNum the input of the clicked number button
+    input.textContent += num;
+    currentNum += num;
+    console.log("current:", currentNum);
 }
 
-function readOperator(e){
-    display.textContent = e.innerText;
-    operator = e.innerText;
-    addNumberArray();
+function readOperator(op){
+    input.textContent += op;
+    operator = op
+    evaluate();
 }
 
-function addNumberArray(){
-    numberContainer.push(inputNumber);
-    inputNumber = '';
-    console.log(numberContainer)
-}
+function evaluate(num) {
+    if (currenNum != '') {
 
-enterBtn.addEventListener("click", () => {
-    addNumberArray();
-    operate(operator, numberContainer[0], numberContainer[1]);
-});
+    }
+    previousNumber = currentNum;
+    currentNum = '';
+    currentNum += num;
+    console.log(`pervious: ${previousNum}, current: ${currentNum}`);
+}
 
 function operate(operator, a, b){
     a = parseInt(a);
     b = parseInt(b);
     switch(operator){
         case '+':
-            display.textContent = a + b;
+            currentResult = a + b;
+            result.textContent = currentResult;
             break;
         case '-':
-            display.textContent = a - b;
+            currentResult = a - b;
+            result.textContent = currentResult;
             break;
         case '*':
-            display.textContent = a * b;
+            currentResult = a * b;
+            result.textContent = currentResult;
             break;
         case '/':
-            display.textContent = a / b;
+            currentResult = a / b;
+            result.textContent = currentResult;
             break;
+    }
+    operator = '';
+
+}
+
+numbers.forEach(btn => {
+    btn.addEventListener("click", () => readInput(btn.innerText))
+})
+
+operatorBtn.forEach(btn => {
+    btn.addEventListener("click", () => readOperator(btn.innerText))
+})
+
+enterBtn.addEventListener("click", () => {
+    fillNumberArray();
+    operate(operator, numberContainer);
+});
+*/
+
+let numberContainer = [];
+let inputNumber = '';
+let operator = '';
+let currentResult = 0;
+
+
+function readValue(num){
+    input.textContent += num;
+    inputNumber += num;
+    fillNumberArray();
+}
+
+function readOperator(op){
+    input.textContent += op;
+    operator = op;
+    console.log(operator);
+}
+
+function fillNumberArray(){
+    if (numberContainer.length < 2) {
+        numberContainer.push(parseInt(inputNumber));
+        inputNumber = '';
+        console.log(numberContainer)
     }
 }
 
-operatorBtn.forEach(btn => {
-    btn.addEventListener("click", () =>{
-        readOperator(btn);
-    })
-})
+function clearDisplay(){
+    input.textContent = '';
+    result.textContent = '';
+    numberContainer = [];
+    console.log(numberContainer);
+}
+
+function checkNextOperator(){
+    if (operator != '') {
+        numberContainer = [];
+        console.log(numberContainer);
+        numberContainer[0] = currentResult;
+        console.log(numberContainer[0]);
+    }
+}
+
+function evaluate(){
+    if (numberContainer.length >= 2) {
+        console.log(true);
+        operate(operator, numberContainer);
+    }
+    
+}
+
+
+function operate(operator, numbers){
+    switch(operator){
+        //after operator switches, change operation from + to - and so forth
+        // brainstorming: turn var operator into array?
+        case '+':
+            currentResult = numbers.reduce((result, nums) => result + nums);
+            result.textContent = currentResult;
+            break;
+        case '-':
+            currentResult = numbers.reduce((result, nums) => result - nums);
+            result.textContent = currentResult;
+            break;
+        case '*':
+            currentResult = numbers.reduce((result, nums) => result * nums);
+            result.textContent = currentResult;
+            break;
+        case '/':
+            currentResult = numbers.reduce((result, nums) => result / nums);
+            result.textContent = currentResult;
+            break;
+    }
+    // empty the operator
+    operator = '';
+    console.log(currentResult);
+    checkNextOperator()
+
+}
+
 
 numbers.forEach(btn => {
     btn.addEventListener("click", () => {
-        readValue(btn);
+        readValue(btn.innerText)
+        evaluate();
     })
 })
+
+operatorBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        readOperator(btn.innerText)
+    })
+})
+
+enterBtn.addEventListener("click", () => {
+    fillNumberArray();
+    operate(operator, numberContainer);
+});
+
+clearBtn.addEventListener("click", () => clearDisplay())

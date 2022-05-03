ba20b26ef000c8df98fcf485dd526ad6f3c5ba96
grinsekatze <violetta.stix@gmail.com>
2022-05-03 12:33:25 +0200

    Fix: Operator handling If enter is pressed before second value is assigned, display first value

--- main.js
+++ main.js
@@ -5,99 +5,50 @@ const input = document.querySelector(".input");
 const result = document.querySelector(".input");
 const clearBtn = document.querySelector(".clear");
 
-/*
-let previousNum = '';
-let currentNum = '';
-let operator = '';
-
-
-function readInput(num){
-    //assign currentNum the input of the clicked number button
-    input.textContent += num;
-    currentNum += num;
-    console.log("current:", currentNum);
-}
-
-function readOperator(op){
-    input.textContent += op;
-    operator = op
-    evaluate();
-}
-
-function evaluate(num) {
-    if (currenNum != '') {
-
-    }
-    previousNumber = currentNum;
-    currentNum = '';
-    currentNum += num;
-    console.log(`pervious: ${previousNum}, current: ${currentNum}`);
-}
-
-function operate(operator, a, b){
-    a = parseInt(a);
-    b = parseInt(b);
-    switch(operator){
-        case '+':
-            currentResult = a + b;
-            result.textContent = currentResult;
-            break;
-        case '-':
-            currentResult = a - b;
-            result.textContent = currentResult;
-            break;
-        case '*':
-            currentResult = a * b;
-            result.textContent = currentResult;
-            break;
-        case '/':
-            currentResult = a / b;
-            result.textContent = currentResult;
-            break;
-    }
-    operator = '';
-
-}
-
-numbers.forEach(btn => {
-    btn.addEventListener("click", () => readInput(btn.innerText))
-})
-
-operatorBtn.forEach(btn => {
-    btn.addEventListener("click", () => readOperator(btn.innerText))
-})
-
-enterBtn.addEventListener("click", () => {
-    fillNumberArray();
-    operate(operator, numberContainer);
-});
-*/
-
 let numberContainer = [];
 let inputNumber = '';
 let operator = '';
 let currentResult = 0;
+let enterPressed = false;
 
 
 function readValue(num){
     input.textContent += num;
     inputNumber += num;
-    //fillNumberArray();
 }
 
 function readOperator(op){
     input.textContent += op;
     operator = op;
-    console.log(operator);
+    //console.log(operator);
 }
 
 function fillNumberArray(){
-    if (numberContainer.length < 2) {
+    if (numberContainer.length < 2 && inputNumber != '') {
         numberContainer.push(parseInt(inputNumber));
         inputNumber = '';
-        console.log(numberContainer)
+        console.log('fill', numberContainer)
+    } else {
+    //if enter is clicked before numberContainer has a second input, display first input
+        console.log(true);
+        result.textContent = numberContainer[0];
     }
     evaluate();
+}
+
+function evaluate(){
+    // might have to call fillNumberArray here, check for enter after operator
+    if (numberContainer.length >= 2) {
+        operate(operator, numberContainer);
+    }
+}
+
+function checkNextOperator(){
+    if (operator != '') {
+        numberContainer = [];
+        numberContainer[0] = currentResult;
+        console.log(numberContainer[0]);
+    }
 }
 
 function clearDisplay(){
@@ -107,27 +58,8 @@ function clearDisplay(){
     console.log(numberContainer);
 }
 
-function checkNextOperator(){
-    if (operator != '') {
-        numberContainer = [];
-        console.log(numberContainer);
-        numberContainer[0] = currentResult;
-        console.log(numberContainer[0]);
-    }
-}
-
-function evaluate(){
-    if (numberContainer.length >= 2) {
-        console.log(true);
-        operate(operator, numberContainer);
-    }
-    
-}
-
 function operate(operator, numbers){
     switch(operator){
-        //after operator switches, change operation from + to - and so forth
-        // brainstorming: turn var operator into array?
         case '+':
             currentResult = numbers.reduce((result, nums) => result + nums);
             result.textContent = currentResult;
@@ -145,32 +77,26 @@ function operate(operator, numbers){
             result.textContent = currentResult;
             break;
     }
-    // empty the operator
     operator = '';
-    console.log(currentResult);
+    console.log('operate', currentResult);
     checkNextOperator()
-
 }
 
-
 numbers.forEach(btn => {
-    btn.addEventListener("click", () => {
-        readValue(btn.innerText)
-    //    evaluate();
-    })
+    btn.addEventListener("click", () => readValue(btn.innerText))
 })
 
 operatorBtn.forEach(btn => {
     btn.addEventListener("click", () => {
         fillNumberArray();
         readOperator(btn.innerText)
-    //    evaluate();
     })
 })
 
 enterBtn.addEventListener("click", () => {
-    fillNumberArray();
-    operate(operator, numberContainer);
-});
+    fillNumberArray()
+    //evaluate();
+    //operate(operator, numberContainer);
+})
 
 clearBtn.addEventListener("click", () => clearDisplay())
